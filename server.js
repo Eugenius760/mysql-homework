f6477b287644e36670a05864bf417f55292a8123
Darryl <williams.de39@gmail.com>
2020-05-23 18:15:21 -0700

    added seeds

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.MYSQL_PASSWORD,
+  password: "abbazabba",
+  //password: process.env.MYSQL_PASSWORD,
   database: "employee_DB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  tracker();
 });
+
+function tracker() {
+    inquirer
+    .prompt({
+        name: "whatToDo",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View All Departments", "View All Roles", "Add Employee", "Add Role", "Add Deparatment", "Update Employee Role"]
+    })
+}
---
+++ utilities/db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name) VALUE ("Sales");
+INSERT INTO department (name) VALUE ("Engineering");
+INSERT INTO department (name) VALUE ("Legal");
+INSERT INTO department (name) VALUE ("Finance");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 70000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Regional Sales Manager", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 80000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Test Engineer", 60000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Lawyer", 110000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Copyright", 90000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 90000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Tax Person", 750000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Darryl", "Williams", 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Abby", "Schuldheisz", 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Maurice", "McClure", 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jay'sen", "Morris", 4, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Tori", "Kirkau", 5, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jenna", "Hahn", 6, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Janaiah", "Lundblade", 7, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Nika", "Rosen", 8, NULL);
